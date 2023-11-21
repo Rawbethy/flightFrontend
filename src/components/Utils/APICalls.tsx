831469688c6edda5082fe61f90e3871d16b412ad
Rawbethy <robert.duque1998@gmail.com>
2023-11-20 23:07:09 -0600

    forgot to save APICalls.tsx

--- src/components/Utils/APICalls.tsx
+++ src/components/Utils/APICalls.tsx
@@ -1,23 +1,37 @@
 import axios from 'axios';
 import {useCookies} from 'react-cookie';
 
-export default function axiosAPIs() {
-    const baseURL = 'http://localhost:8080';
-    const [cookies] = useCookies(['token'])
+const baseURL = 'http://localhost:8080';
+
+const createInstance = (url: string, token: string | null) => {
+  const newURL = `${url !== '' ? `${baseURL}/${url}` : `${baseURL}`}`
+  return axios.create({
+    baseURL: `${newURL}`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${token}` 
+    }
+  });
+};
+
+export default createInstance;
+
+// export default function axiosAPIs() {
+//     const baseURL = 'http://localhost:8080';
     
-    const createInstance = (url: string, token: string | null) => {
-        const newURL = `${url !== '' ? `${baseURL}/${url}` : `${baseURL}`}`
-        return axios.create({
-          baseURL: `${newURL}`,
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `${token}` 
-          }
-        });
-    };
-    const airlineAPI = createInstance('url', null);
-    const registerAPI = createInstance('register', cookies.token);
-    const loginAPI = createInstance('login', cookies.token);
-    const getPricesAPI = createInstance('getPrices', cookies.token);
-    const getHistoryAPI = createInstance('getHistory', cookies.token);
-}
+//     const createInstance = (url: string, token: string | null) => {
+//         const newURL = `${url !== '' ? `${baseURL}/${url}` : `${baseURL}`}`
+//         return axios.create({
+//           baseURL: `${newURL}`,
+//           headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `${token}` 
+//           }
+//         });
+//     };
+//     const airlineAPI = createInstance('url', null);
+//     const registerAPI = createInstance('register', cookies.token);
+//     const loginAPI = createInstance('login', cookies.token);
+//     const getPricesAPI = createInstance('getPrices', cookies.token);
+//     const getHistoryAPI = createInstance('getHistory', cookies.token);
+// }
